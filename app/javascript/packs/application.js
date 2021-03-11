0a3667eb34a67da6e329bfc480f315f77556ed90
MK8-31 <popofst@yahoo.co.jp>
2021-03-11 10:02:10 +0900

    add jquery to application.js and environment.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,17 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+require("@rails/ujs").start()
+require("turbolinks").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+import "bootstrap"
+require("chartkick")
+require("chart.js")
+require("bootstrap")
+
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
