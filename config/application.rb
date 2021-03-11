93b3fe0f3f0a4fc19be57642d6e048c7fd56585f
MK8-31 <popofst@yahoo.co.jp>
2021-03-11 10:25:15 +0900

    add japanese

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,25 @@ Bundler.require(*Rails.groups)
 module StudyAppAws
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 6.0
+    
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    # Settings in config/environments/* take precedence over those specified here.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
+    config.time_zone = 'Tokyo'
+
+    config.generators do |g|
+      g.test_framework :rspec, 
+            view_specs: false, 
+            helper_specs: false, 
+            controller_specs: true, 
+            routing_specs: false,
+            request_specs: false
+    end
   end
 end
